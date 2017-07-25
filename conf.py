6de6b9185855ead12c13039ca974e754bade8c33
hnorman100 <hnorman100@googlemail.com>
2017-07-25 10:20:41 +0100

    Add pages directory
    Now it is all lovely and clean.

--- conf.py
+++ conf.py
@@ -43,7 +43,7 @@ templates_path = ['_templates']
 source_suffix = '.rst'
 
 # The master toctree document.
-master_doc = 'index'
+master_doc = 'pages/index'
 
 # General information about the project.
 project = 'SCD Cloud Guide'
